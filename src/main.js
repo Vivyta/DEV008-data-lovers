7bb11587686ba4149c7b74641d7c13ab0acbae62
Vivyta <vivilandiia@gmail.com>
2023-06-22 09:22:50 -0400

    agrego imagenes, cambios en html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+console.log(data);
+// const datapokemon = data.pokemon;
+// console.log(datapokemon);
+// for (let i = 0; i <= datapokemon.length; i++) {
+//     console.log(datapokemon[i]);
 
-console.log(example, data);
+
+// }
+// const findElement = (datos, searchedname) => {
+//     let elementpokemon = [];
+//     for (let i = 0; i < datos.length; i++) {
+//         const element = datos[i];
+//         if (element.name === searchedname) {
+
+//             console.log(element)
+//         }
+//     }
+//     return elementpokemon
+// }
+// findElement(datapokemon, "pikachu")
+function getItemsHtml(items) {
+  let htmlContent = "";
+  items.forEach((element) => {
+    htmlContent+= '';
+   <div class="item">
+      <img src = "${data.pokemon.js/img}">
+      <div>
+       <h4>${element.name} ${Element.num}</h4>
+    </div>
+   </div>
+   ;
+  });
+  return htmlContent;
+
+}
+document.getElementById("datapokemon.js").innerHTML=getItemsHtml(data.items)
